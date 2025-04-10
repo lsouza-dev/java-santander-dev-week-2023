0bebf531ce361cf33eeac1f9f9997207962bab7d
Luiz Fabiano de Souza <luizfabianods04@gmail.com>
2025-04-10 10:32:05 -0300

    Primeira tentativa de Deploy no Railway

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,13 +1,22 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("PGHOST: " + System.getenv("PGHOST"));
+		System.out.println("PGPORT: " + System.getenv("PGPORT"));
+		System.out.println("PGDATABASE: " + System.getenv("PGDATABASE"));
+		System.out.println("PGUSER: " + System.getenv("PGUSER"));
+		System.out.println("PGPASSWORD: " + System.getenv("PGPASSWORD"));
+
 	}
 
 }
